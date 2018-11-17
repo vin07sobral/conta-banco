35815b3cc4acef99811035deb8d73a1cb25882c0
Marcus Vinicius Sobral <mvinicius.sobral@gmail.com>
2018-11-17 09:52:19 -0200

    Add files via upload

---
+++ Criar Conta/Criar Conta/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+
+namespace Criar_Conta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            conta x ;
+            
+
+            Console.Write(" Digite o numero da Conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write(" Digite o nome do Titular: ");
+            string nome = Console.ReadLine();
+            Console.Write("Havera deposito inicial (s/n)? ");
+            char ch = char.Parse(Console.ReadLine());
+            if (ch == 's')
+            {
+                Console.Write("Digite o valor inicial:");
+                double valorInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                x = new conta(numero, nome, valorInicial);
+            }
+            else
+            {
+                x = new conta(numero, nome);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Conta Criada: ");
+            Console.WriteLine(x);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite um valor para deposito: ");
+            double dep = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.depositar(dep);
+            Console.WriteLine();
+            Console.WriteLine("Conta Atualizada: ");
+            Console.WriteLine(x);
+
+            Console.WriteLine();
+            Console.Write("Digite o valor para saque: ");
+            double saq = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.sacar(saq);
+            Console.WriteLine();
+            Console.WriteLine("Conta Atualizada: ");
+            Console.WriteLine(x);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Criar Conta/Criar Conta/conta.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Criar_Conta
+{
+    class conta
+    {
+        public int numero { get; private set; }
+        public string titular { get; set; }
+        public double saldo { get; private set; }
+
+        public conta(int numero, string titular, double saldo)
+        {
+            this.numero = numero;
+            this.titular = titular;
+            this.saldo = saldo;
+          
+        }
+
+        public conta(int numero, string titular)
+        {
+            this.numero = numero;
+            this.titular = titular;
+            this.saldo = 0.0;
+        }
+
+        public void depositar(double quantia)
+        {
+            saldo += quantia;
+        }
+
+        public void sacar(double quantia)
+        {
+            saldo = saldo - quantia - 5.0;
+        }
+
+        public override string ToString()
+        {
+            return "Conta" +
+                numero
+                + "Titular: "
+                + titular
+                + ", Saldo: $"
+                + saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
